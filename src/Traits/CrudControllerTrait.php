d2dbfcc4e87da5382f21c44e5904664d0e51de1d
Abu Baker Siddique <abskomol@gmail.com>
2017-05-11 17:47:02 +0600

    Controller Stub update, Crud controller trait update

--- src/Traits/CrudControllerTrait.php
+++ src/Traits/CrudControllerTrait.php
@@ -2,6 +2,7 @@
 
 namespace Tirjok\CrudGenerator\Traits;
 
+use Illuminate\Support\Facades\Session;
 
 trait CrudControllerTrait
 {
@@ -12,9 +13,9 @@ trait CrudControllerTrait
      */
     public function index()
     {
-        $$this->crudName = $this->service->all();
+        ${$this->crudName} = $this->service->all();
 
-        return view($this->viewPath . $this->viewName . '.index', compact($$this->crudName));
+        return view($this->viewPath . $this->viewName . '.index', compact("{$this->crudName}"));
     }
 
 
@@ -38,14 +39,58 @@ trait CrudControllerTrait
      */
     public function show($id)
     {
-        $$this->crudNameSingular = $this->service->find($id);
+        ${$this->crudNameSingular} = $this->service->find($id);
 
-        if (!$$this->crudNameSingular) {
+        if (!${$this->crudNameSingular}) {
             Session::flash('flash_error', $this->modelName . ' not found!');
 
             return redirect("{$this->routeGroup}{$this->viewName}");
         }
 
-        return view($this->viewPath.$this->viewName.'.show', compact($$this->crudNameSingular));
+        return view($this->viewPath.$this->viewName . '.show', compact("{$this->crudNameSingular}"));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function edit($id)
+    {
+        ${$this->crudNameSingular} = $this->service->find($id);
+
+        if (!${$this->crudNameSingular}) {
+        Session::flash('flash_error', $this->modelName.' not found!');
+
+            return redirect("{$this->routeGroup}{$this->viewName}");
+    }
+
+        return view($this->viewPath.$this->viewName . '.edit', compact("{$this->crudNameSingular}"));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        try {
+            $is_deleted = $this->service->delete($id);
+
+            if ($is_deleted) {
+                Session::flash('flash_message', '{{modelName}} deleted!');
+            }
+
+        } catch (\Exception $e) {
+            Session::flash('flash_error', 'There is no data regarding this id');
+        }
+
+        return redirect("{$this->routeGroup}{$this->viewName}");
     }
 }
